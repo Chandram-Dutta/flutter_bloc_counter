c698724881a2c3dd7ebd6f97f6ed78b7248b6977
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-10-16 15:31:07 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,63 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'app.dart';
-import 'counter_observer.dart';
+import 'cubit/counter_cubit.dart';
 
-void main() {
-  Bloc.observer = CounterObserver();
-  runApp(const CounterApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BLoC',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter BLoC'),
+        ),
+        body: const Col(),
+      ),
+    );
+  }
+}
+
+class Col extends StatelessWidget {
+  const Col({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: BlocBuilder<CounterCubit, CounterState>(
+        builder: (context, state) {
+          return Column(
+            children: [
+              Text(state.counterValue.toString()),
+              Row(
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: "Increment",
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: "Decrement",
+                    child: const Icon(Icons.add),
+                  )
+                ],
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
